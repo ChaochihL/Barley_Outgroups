e7379589d7e8ac044550a9c81a8d2f667fbbbfec
Chaochih Liu <liux1299@umn.edu>
2017-05-05 11:41:12 -0500

    fixed where stampy looks for ref file

--- stampy_mapped/test_stampy-bulbosum.sh
+++ stampy_mapped/test_stampy-bulbosum.sh
@@ -16,16 +16,28 @@ module load stampy_ML/1.0.31
 #   User provided arguments
 #   Reference prefix must match .stidx files
 REF_PREFIX='barley_pseudomolecules_parts'
+#   What directory contains our reference files?
+REF_DIR='/home/morrellp/liux1299/scratch/outgroups'
+#   What is our per site substitution rate?
 DIVERGENCE='0.04'
+#   Where do our output files go?
+OUT_DIR=
+#   What is our forward read?
 FORWARD='/home/morrellp/liux1299/scratch/outgroups/Adapter_Trimming/bulbosum02_R1_trimmed.fastq.gz'
+#   What is our reverse read?
 REVERSE='/home/morrellp/liux1299/scratch/outgroups/Adapter_Trimming/bulbosum02_R2_trimmed.fastq.gz'
+#   Takes our forward read and generates sample name for output file
+SAMPLE_NAME="$(basename ${FORWARD} | cut -f 1 -d '.')"
 
 #   Stampy test run
 #       -g is the genome index file, PREFIX.stidx
 #       -h is the genome hash file, PREFIX.sthash
 #       --substitutionrate is the expected fraction of Poisson-distributed substitutions (default is 0.001)
 #       -T runs both: -S command to simulate reads and -P command to parse .sam output
+#       -o is our output file
+cd ${REF_DIR}
 stampy -g "${REF_PREFIX}" \
        -h "${REF_PREFIX}" \
        --substitutionrate="${DIVERGENCE}" \
        -T "${FORWARD}","${REVERSE}"
+       -o "${OUT_DIR}/${SAMPLE_NAME}.sam"
