0d0866368aafd96da9626e4df180edb8b17ab71f
Chaochih Liu <liux1299@umn.edu>
2017-05-18 14:46:03 -0500

    script to run angsd-wrapper ancestral seq

---
+++ ANGSD-wrapper/Ancestral_pubiflorum_0.09.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#PBS -l mem=22gb,nodes=1:ppn=16,walltime=24:00:00
+#PBS -m abe
+#PBS -M liux1299@umn.edu
+#PBS -q lab
+
+set -e
+set -o pipefail
+
+module load R/3.3.3
+
+~/software_development/angsd-wrapper/angsd-wrapper Ancestral /home/morrellp/liux1299/GitHub/Barley_Outgroups/ANGSD-wrapper/Ancestral_Sequence_pubiflorum_0.09_Config
