77dccc30de755d77b53b222276ba6dd6b00a0257
liux1299 <liux1299@umn.edu>
2018-01-29 21:19:51 -0600

    convert murinum bam to fasta

---
+++ Bam_to_Fasta/Ancestral_murinum_0.03.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#PBS -l mem=62gb,nodes=1:ppn=24,walltime=72:00:00
+#PBS -m abe
+#PBS -M liux1299@umn.edu
+#PBS -q small
+
+set -e
+set -o pipefail
+
+module load R/3.3.3
+
+~/software_development/angsd-wrapper/angsd-wrapper Ancestral /home/morrellp/liux1299/GitHub/Barley_Outgroups/ANGSD-wrapper/Ancestral_Sequence_murinum_0.03_Config
