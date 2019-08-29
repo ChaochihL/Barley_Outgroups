84d7174d79d74e641696d68096a5e2d72e6de411
ChaochihL <liux1299@umn.edu>
2019-08-29 17:33:25 -0500

    stampy map pubiflorum

---
+++ morex_v2/01_mapping/stampy_mapped/stampy-pubiflorum-0.09.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#PBS -l mem=60gb,nodes=1:ppn=24,walltime=90:00:00
+#PBS -m abe
+#PBS -M liux1299@umn.edu
+#PBS -q mesabi
+
+set -e
+set -o pipefail
+
+#   This script maps barley outgroup sequences and outputs a SAM file.
+#   This script was written by Chaochih Liu
+
+module load stampy_ML/1.0.32
+
+#   User provided arguments
+#   Reference prefix must match .stidx files
+REF_PREFIX='Barley_Morex_V2_pseudomolecules_parts'
+#   What directory contains our reference files?
+REF_DIR='/home/morrellp/liux1299/Shared/References/Reference_Sequences/Barley/Morex_v2/stampy_files'
+#   What is our per site substitution rate?
+DIVERGENCE='0.09'
+#   Where do our output files go?
+OUT_DIR='/scratch.global/liux1299/barley_outgroups/stampy_mapped'
+#   What is our forward read?
+FORWARD='/home/morrellp/liux1299/Alignments/barley_outgroups/Adapter_Trimming/pubiflorum_BCC2028_Forward_ScytheTrimmed.fastq.gz'
+#   What is our reverse read?
+REVERSE='/home/morrellp/liux1299/Alignments/barley_outgroups/Adapter_Trimming/pubiflorum_BCC2028_Reverse_ScytheTrimmed.fastq.gz'
+#   Takes our forward read and generates sample name for output file
+SAMPLE_NAME="pubiflorum_BCC2028"
+
+#   Stampy test run
+#       -g is the genome index file, PREFIX.stidx
+#       -h is the genome hash file, PREFIX.sthash
+#       --substitutionrate is the expected fraction of Poisson-distributed substitutions (default is 0.001)
+#       -f is the output file format
+#       -M reads to map
+#       -o is our output file
+cd ${REF_DIR}
+stampy -g "${REF_PREFIX}" \
+       -h "${REF_PREFIX}" \
+       --substitutionrate="${DIVERGENCE}" \
+       -f sam \
+       -o "${OUT_DIR}/${SAMPLE_NAME}_0.09.sam" \
+       -M "${FORWARD}","${REVERSE}"
+
